973212f9043320ebf511154edbfb5f0136066026
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-07-04 05:37:15 +0800

    fix bootimg building, overlay files for unity-system-compositor, display size, brightness control

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -xe
 
+chmod 755 mkbootimg_modified/mkbootimg
+export PATH=$(pwd)/mkbootimg_modified:$PATH
+
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
 ./build/build.sh "$@"
