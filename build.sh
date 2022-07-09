441fe69c74b568b3543529122473f2dcf55408b7
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-07-09 23:05:10 +0800

    load forked halium-generic-adaptation-build-tools until features are upstreamed

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -xe
 chmod 755 mkbootimg_modified/mkbootimg
 export PATH=$(pwd)/mkbootimg_modified:$PATH
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+# until some features are merged, use forked version
+#[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+[ -d build ] || git clone https://gitlab.com/kethen/halium-generic-adaptation-build-tools.git -b halium-11 build
+
 ./build/build.sh "$@"
