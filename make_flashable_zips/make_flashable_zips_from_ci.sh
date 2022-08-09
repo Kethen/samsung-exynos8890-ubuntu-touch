c7f559015951f79d24438621ed6f1e92cebba2c8
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-08-09 14:46:54 +0800

    create ci flashable from devel-flashable

--- make_flashable_zips/make_flashable_zips_from_ci.sh
+++ make_flashable_zips/make_flashable_zips_from_ci.sh
@@ -10,17 +10,20 @@ for model in herolte hero2lte
 do
 	rm -rf *
 
-	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/boot.img?job=flashable-${model} -O boot.img
-	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/recovery.img?job=flashable-${model} -O recovery.img
-	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/system.img?job=flashable-${model} -O system.simg
+	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/boot.img?job=devel-flashable-${model} -O boot.img
+	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/recovery.img?job=devel-flashable-${model} -O recovery.img
+	#wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/system.img?job=devel-flashable-${model} -O system.simg
+	wget https://gitlab.com/ubports/porting/community-ports/android11/samsung-galaxy-s7/samsung-exynos8890/-/jobs/artifacts/main/raw/out/ubuntu.img?job=devel-flashable-${model} -O system.img
 
-	simg2img system.simg system.img
+	#simg2img system.simg system.img
 
 	mkdir -p META-INF/com/google/android
 	cp "$srcdir"/template/META-INF/com/google/android/* META-INF/com/google/android/
 	sed -i "/#!\/sbin\/sh/a MODEL=$model" META-INF/com/google/android/update-binary
-	zip -r -y -9 "$outputdir"/${model}-$(date +%Y-%m-%d).zip META-INF system.img boot.img
-	zip -r -y -9 "$outputdir"/${model}-$(date +%Y-%m-%d)-with-recovery.zip META-INF system.img boot.img recovery.img
+	date=$(date +%Y-%m-%d)
+	zip -r -y -9 "$outputdir"/${model}-${date}.zip META-INF system.img boot.img
+	cp "$outputdir"/${model}-${date}.zip "$outputdir"/${model}-${date}-with-recovery.zip
+	zip -r -y -9 "$outputdir"/${model}-${date}-with-recovery.zip recovery.img
 done
 
 cd ../
