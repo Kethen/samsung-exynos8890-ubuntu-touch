f391a8b7a11de521f7ad9556e53e359eb8c6f203
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-09-21 05:06:11 +0800

    enable gpu rasterization on qtwebegine, and zero copy for when it decides to fallback to cpu

---
+++ overlay_template/system/rootfs_overlay/etc/profile.d/disable-qtwebengine-gpu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This file was part of lxc-android-config, overriden here
+# gpu rasterization seems very usable on this device
+
+# leaving viz disabled according to https://github.com/ubports/lxc-android-config/commit/bf7c2dc380543dc9369f1ce8bb4b478729727636
+
+export QTWEBENGINE_CHROMIUM_FLAGS="--enable-gpu-rasterization --enable-zero-copy --disable-viz-display-compositor"
+
+# uncomment this to just use zero copy instead, which gives a big boost to scrolling framerate as well
+#export QTWEBENGINE_CHROMIUM_FLAGS="--enable-zero-copy --disable-viz-display-compositor"
