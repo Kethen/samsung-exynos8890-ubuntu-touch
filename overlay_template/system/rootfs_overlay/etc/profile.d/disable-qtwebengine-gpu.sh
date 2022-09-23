9904152abea2620ff4fb35891bcac5877e7832e7
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-09-23 20:59:56 +0800

    use OverlayScrollbar in Morph

--- overlay_template/system/rootfs_overlay/etc/profile.d/disable-qtwebengine-gpu.sh
+++ overlay_template/system/rootfs_overlay/etc/profile.d/disable-qtwebengine-gpu.sh
@@ -8,3 +8,6 @@ export QTWEBENGINE_CHROMIUM_FLAGS="--enable-gpu-rasterization --enable-zero-copy
 
 # uncomment this to just use zero copy instead, which gives a big boost to scrolling framerate as well
 #export QTWEBENGINE_CHROMIUM_FLAGS="--enable-zero-copy --disable-viz-display-compositor"
+
+# enable OverlayScrollbar
+export QTWEBENGINE_CHROMIUM_FLAGS="$QTWEBENGINE_CHROMIUM_FLAGS --enable-features=OverlayScrollbar"
