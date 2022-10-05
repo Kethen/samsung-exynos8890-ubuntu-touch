602feb77bd7922ac0735ed13ec2d4b0589c61249
Katharine Chui <kwchuiaa@connect.ust.hk>
2022-10-05 21:33:19 +0800

    add autosleep enter delay tunable

--- overlay_template/system/rootfs_overlay/usr/sbin/autosleep_delay_set.sh
+++ overlay_template/system/rootfs_overlay/usr/sbin/autosleep_delay_set.sh
@@ -3,9 +3,11 @@
 if [ "$1" == "charging" ]
 then
 	echo 10 > /sys/power/autosleep_delay
+	echo 25 > /sys/power/autosleep_enter_delay
 fi
 
 if [ "$1" == "discharging" ]
 then
 	echo 2 > /sys/power/autosleep_delay
+	echo 5 > /sys/power/autosleep_enter_delay
 fi
