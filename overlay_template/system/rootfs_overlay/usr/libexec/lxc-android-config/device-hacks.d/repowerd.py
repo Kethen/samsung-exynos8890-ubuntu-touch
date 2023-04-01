c01b4da7b69c9a248a4948bad4d014e438292e82
Katharine Chui <kwchuiaa@connect.ust.hk>
2023-04-01 21:16:25 +0800

    aidl_perf_boost: listSysRequests prototyping

--- overlay_template/system/rootfs_overlay/usr/libexec/lxc-android-config/device-hacks.d/repowerd.py
+++ overlay_template/system/rootfs_overlay/usr/libexec/lxc-android-config/device-hacks.d/repowerd.py
@@ -30,7 +30,8 @@ def init(bus, log):
 			break
 		except Exception as e:
 			log_cb(e)
-			time.sleep(1)
+			if __name__ != "__main__":
+				time.sleep(1)
 			tries = tries + 1
 			if tries > 20:
 				raise Exception("could not connect to repowerd through dbus")
@@ -69,10 +70,40 @@ def clearWakelock(cookie):
 		log_cb(e)
 		return False
 
+def requestDisplayOn():
+	try:
+		cookie = unity_screen_interface.keepDisplayOn()
+		return cookie
+	except Exception as e:
+		log_cb(e)
+		return None
+
+def clearDisplayOn(cookie):
+	try:
+		unity_screen_interface.removeDisplayOnRequest(cookie)
+		return True
+	except Exception as e:
+		log_cb(e)
+		return False
+
+# mode as string "on" or "off"
+enum_reason_notification = 4
+enum_reason_snap_decision = 5
+def setScreenPowerMode(mode, reason):
+	try:
+		ret = unity_screen_interface.setScreenPowerMode(mode, reason)
+		return ret
+	except Exception as e:
+		log_cb(e)
+		return False
+
 def getWakelockList():
 	try:
 		val = repowerd_interface.listSysRequests();
-		return val
+		if "request_sys_state" in val:
+			return val["request_sys_state"]
+		log_cb("warning: request_sys_state not found in listSysRequests dump")
+		return None
 	except Exception as e:
 		log_cb(e)
 		return None
@@ -91,3 +122,55 @@ def wakeup_cb_wrapper(*arg, **karg):
 def register_wakeup_cb(cb):
 	global wakeup_cb
 	wakeup_cb = cb
+
+def main_log(message):
+	print(message)
+
+def main():
+	bus = dbus.SystemBus()
+	try:
+		init(bus, main_log)
+	except Exception as e:
+		print(e)
+
+	print("requesting wakelock")
+	wake_cookies = []
+	display_on_cookies = []
+	for f in range(0,3):
+		print("requesting wakelock #{0}".format(f))
+		cookie = requestWakelock("test {0}".format(f))
+		print("wakelock #{0} has cookie {1}".format(f, cookie))
+		wake_cookies.append(cookie)
+		print("requesting displayon #{0}".format(f))
+		cookie = requestDisplayOn()
+		print("displayon #{0} has cookie {1}".format(f, cookie))
+		display_on_cookies.append(cookie)
+		setScreenPowerMode("on", enum_reason_notification)
+
+	time.sleep(5)
+
+	print(daemonHasActiveWakelock())
+
+	for cookie in wake_cookies:
+		print("releasing wakelock with cookie {0}".format(cookie))
+		clearWakelock(cookie)
+		time.sleep(5)
+
+	print(daemonHasActiveWakelock())
+
+	for cookie in display_on_cookies:
+		print("releasing display on with cookie {0}".format(cookie))
+		clearDisplayOn(cookie)
+		time.sleep(5)
+
+	print(daemonHasActiveWakelock())
+
+	for f in range(0,3):
+		print("releasing notifications")
+		setScreenPowerMode("off", enum_reason_snap_decision)
+		time.sleep(5)
+
+	print(daemonHasActiveWakelock())
+
+if __name__ == "__main__":
+	main()
